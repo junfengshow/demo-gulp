3c72f4939f91a3fe422c237e2d5e9748b5afb86b
wjf01009320 <wjf01009320@alibaba-inc.com>
2021-05-07 10:49:10 +0800

    babel方入webpack配置文件中

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 const { series, src, dest, watch } = require('gulp') 
 // ---------------------------------------------------------
 // 使用gulp-babel进行js的转换
-const babel = require('gulp-babel')
+const gulpBabel = require('gulp-babel')
 // 使用gulp-rev对资源文件加hash
 const rev = require('gulp-rev')
 // 对html替换成加了hash的文件名
@@ -16,31 +16,47 @@ const gulpClean = require('gulp-clean')
 // web 服务
 const connect = require('gulp-connect')
 // 使用webpack进行打包 也可以选择browserify等其它工具
-const webpack = require('webpack-stream')
+const webpackStream = require('webpack-stream')
 // 命名并输出文件系统
 const named = require('vinyl-named')
 // 中html插入js、css、html等
 const gulpInject = require('gulp-inject')
 // 链接插件
 const concat = require('gulp-concat')
+const through2 = require('through2');
 
 // ---------------------------------------------------------
 // 处理js
 function scriptsTask () {
-  // js 文件地址
-  return src('./src/**/*.js')
+  // 文件内import进去的文件也需要使用babel进行转换
+  // 故而将babel的配置移入到webpack中
+  return src('./src/main.js')
   // babel 转换
-  .pipe(babel({
-    plugins: ['@babel/plugin-proposal-class-properties'],
-    presets: ['@babel/preset-react', '@babel/preset-env']
-  }))
+  // .pipe(gulpBabel({
+  //   plugins: ['@babel/plugin-proposal-class-properties'],
+  //   presets: ['@babel/preset-react', '@babel/preset-env']
+  // }))
   // 先进性命名、如果不命名webpack会将各个文件合并成一个文件
   .pipe(named())
   // 使用webpack进行打包
-  .pipe(webpack({
-    // mode: 'development',
+  .pipe(webpackStream({
+    mode: 'development',
     output: {
       filename: '[name].js'
+    },
+    plugins: [],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@babel/plugin-proposal-class-properties'],
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      ]
     }
   }))
   // rev生成带hash文件 
@@ -122,6 +138,6 @@ function webServer (cb) {
 
 // ---------------------------------------------------------
 // 设置监听
-// watch(['src/**/*.js', 'src/**/*.css'], null, series(scriptsTask,cssTask, collectorTask()))
+watch(['src/**/*.js', 'src/**/*.css'], null, series(scriptsTask, cssTask, collectorTask()))
 
-exports.default = series(cleanTask, scriptsTask, cssTask, collectorTask())
+exports.default = series(cleanTask, scriptsTask, cssTask, collectorTask(), webServer)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Button } from './components'
 
 class Main extends React.Component {
   constructor (props) {
@@ -12,7 +13,9 @@ class Main extends React.Component {
   }
   render () {
     return (
-      <div>this is main</div>
+      <div>
+        <Button />
+      </div>
     )
   }
 }
